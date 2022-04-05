db2a6522e9335493c96db3c8b3a913dd2935a1e9
walentina97 <rusakovva97@gmail.com>
2022-04-05 10:16:59 +0300

    js bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -18,41 +18,29 @@ const copy = () => {
 };
 
 const copyjs = () => {
-  return src(["app/bootstrap.bundle.js"]).pipe(dest("./build"));
+  return src(["node_modules/bootstrap/dist/js/bootstrap.bundle.js"]).pipe(
+    dest("./build")
+  );
 };
 
-// Перенос скриптов из node_modules в директорию dist/js
-const scripts = () => {
-  return src([
-    "./node_modules/jquery/build/jquery.min.js",
-    // Здесь остальные скрипты
-  ]).pipe(dest("./build/js/"));
-};
-// Компиляция SASS в единый CSS файл
 const sass2css = () => {
-  return src([
-    "app/scss/app.scss",
-    // Путь к главному файлу scss, который будет компилироваться
-  ])
+  return src(["app/scss/app.scss"])
     .pipe(sass())
     .pipe(dest("./build/styles"))
     .pipe(browserSync.stream());
-  // Обработка через плагин sass, указание конечного файла и его месторасположение
 };
-// Компиляция Pug. Принцип работы такой же, как и у компиляции SASS
+
 const pug2html = () => {
-  return src(["app/index.pug", "app/chat.pug"])
+  return src(["app/pug/index.pug", "app/pug/chat.pug"])
     .pipe(pug())
     .pipe(dest("./build/"))
     .pipe(browserSync.stream());
 };
 
-// Создание svg спрайта из иконок
 const svg2sprite = () => {
   const config = {
     mode: {
       stack: {
-        // Activate the «css» mode
         sprite: "../sprite.svg",
       },
     },
@@ -66,7 +54,7 @@ const svg2sprite = () => {
 const startWatch = () => {
   watch(["app/gulpfile.js"], scripts);
   watch(["app/scss/app.scss"], sass2css);
-  watch(["app/chat.pug", "app/index.pug"], pug2html);
+  watch(["app/pug/chat.pug", "app/pug/index.pug"], pug2html);
 };
 exports.browsersync = browsersync;
 exports.default = parallel(pug2html, sass2css, svg2sprite, copy, copyjs);
